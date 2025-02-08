41d303c40aaec28e901506c629ba5f2086179299
Augusto <augustotemplar11@gmail.com>
2025-02-08 15:59:37 +0100

    Aggiungi un menu a discesa per l'utente autenticato nella navbar e implementa la funzionalità di logout.

--- laravel/resources/views/layouts/main.blade.php
+++ laravel/resources/views/layouts/main.blade.php
@@ -59,9 +59,21 @@
                         </li>
                         @if (Route::has('login'))
                             @auth
-                                <li class="nav-item">
-                                    <a href="{{ route('home') }}" class="nav-link">Home</a>
-                                </li>
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
                             @else
                                 <li class="nav-item">
                                     <a href="{{ route('login') }}" class="btn btn-primary">Log in</a>
