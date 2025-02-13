8ae976164018ebcd728920c49dfc922fc49ad359
Augusto <augustotemplar11@gmail.com>
2025-02-13 17:48:47 +0100

    Correggi il percorso dell'azione di approvazione degli eventi nel modulo di approvazione

--- laravel/resources/views/admin/pending.blade.php
+++ laravel/resources/views/admin/pending.blade.php
@@ -9,7 +9,7 @@
                 <h5 class="card-title">{{ $event->title }}</h5>
                 <p class="card-text">{{ $event->description }}</p>
                 <a href="{{ route('events.show', $event->id) }}" class="btn btn-primary">Visualizza</a>
-                <form action="{{ route('events.approve', $event->id) }}" method="POST" class="d-inline">
+                <form action="{{ route('admin.approve', $event->id) }}" method="POST" class="d-inline">
                     @csrf
                     @method('PATCH')
                     <button type="submit" class="btn btn-success">Approva</button>
