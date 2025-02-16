f5a717bf4fb810dc3138826bd7222c1e437f685f
il_peppone <pepperusso07@gmail.com>
2025-02-16 22:02:15 +0100

    Aggiungi un modello di email per gli aggiornamenti e modifica lo stile della sezione di benvenuto

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
    <!-- Hero Section -->
 <section class="hero-section text-center text-white" style="position: relative; padding: 100px 0;">
     <img src="{{ asset('images/log_background.jpeg') }}" alt="Hero Background" class="img-fluid w-100" style="position: absolute; top: 0px; left: 0; width: 100%; height: 100%; object-fit: cover; filter: blur(10px);">
-    <div class="container" style="position: relative; z-index: 1; color: white; background-color: rgba(10, 0, 0, 0.8); border-radius: 10px; padding: 20px;">
+    <div class="container" style="position: relative; z-index: 1; color: rgba(10, 0, 0, 0.8); background-color: rgba(255, 255, 255, 0.692) ; border-radius: 10px; padding: 20px;">
         <h1 class="display-4">Benvenuto su Torbit</h1>
         <p class="lead">Scopri gli eventi più interessanti a Ferrara, organizzati sia da te che dagli altri utenti!</p>
         <div class="d-flex justify-content-center align-items-center mt-4">
