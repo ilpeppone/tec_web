676c9e52c15628930fd0c0b3b233bf12830fab8a
Augusto <augustotemplar11@gmail.com>
2025-02-17 20:48:51 +0100

    Aggiorna i permessi dei file e modifica il factory degli eventi per utilizzare un'immagine di default

--- laravel/database/factories/EventFactory.php
+++ laravel/database/factories/EventFactory.php
@@ -6,9 +6,7 @@ use App\Models\Event;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Storage;
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Event>
- */
+
 class EventFactory extends Factory
 {
     protected $model = Event::class;
@@ -22,23 +20,21 @@ class EventFactory extends Factory
     {
         $this->faker = \Faker\Factory::create('it_IT');
 
-    // Ottieni tutti i file dalla cartella public/images/events
-    $imageFiles = Storage::disk('public')->files('images/events');
+        // Crea un percorso di immagine di default
+        $randomImage = 'events/default.jpg'; // esempio di immagine
 
-    // Scegli un file a caso se esistono file nella cartella, altrimenti usa un placeholder
-    $randomImage = !empty($imageFiles) ? 'storage/' . $this->faker->randomElement($imageFiles) : 'storage/images/placeholder.jpg';
 
-    return [
-        'user_id' => \App\Models\User::factory(),
-        'title' => $this->faker->realText(20),
-        'description' => $this->faker->realText(400),
-        'image_path' => $randomImage,
-        'is_outdoor' => $this->faker->boolean,
-        'event_date' => $this->faker->dateTimeBetween('+1 week', '+1 year'),
-        'max_participants' => $this->faker->numberBetween(10, 100),
-        'address' => $this->faker->address,
-        'price' => $this->faker->randomFloat(2, 0, 10),
-        'approved' => $this->faker->boolean(90),
-    ];
+        return [
+            'user_id' => \App\Models\User::factory(),
+            'title' => $this->faker->realText(20),
+            'description' => $this->faker->realText(400),
+            'image_path' => $randomImage, // Utilizza l'immagine di default
+            'is_outdoor' => $this->faker->boolean,
+            'event_date' => $this->faker->dateTimeBetween('+1 week', '+1 year'),
+            'max_participants' => $this->faker->numberBetween(10, 100),
+            'address' => $this->faker->address,
+            'price' => $this->faker->randomFloat(2, 0, 10),
+            'approved' => $this->faker->boolean(90),
+        ];
     }
 }
