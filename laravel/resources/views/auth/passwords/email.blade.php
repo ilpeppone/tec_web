91fccad6990bb5683fd92593f5997a13dcc1816a
Augusto <augustotemplar11@gmail.com>
2025-02-18 20:52:15 +0100

    aggiorna i layout delle pagine di reset della password per utilizzare il layout principale e migliora la formattazione del contenuto

--- laravel/resources/views/auth/passwords/email.blade.php
+++ laravel/resources/views/auth/passwords/email.blade.php
@@ -1,13 +1,14 @@
-@extends('layouts.main')
+@extends('layouts.app')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
+        <div class="container" style="position: relative; z-index: 1; background-color: #6d6d6d; border-radius: 10px; padding: 20px;">
+        
+            <h1 class="display-4 text-center">Reset Password</h1>
+            <p class="lead text-center">Per favore fornisci la tua email per resettare la tua password</p>
+                <div>
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
@@ -18,7 +19,7 @@
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Indirizzo E-mail') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -33,8 +34,8 @@
 
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
+                                <button type="submit" class="btn btn-custom-pri">
+                                    {{ __('Invia link per il reset della password') }}
                                 </button>
                             </div>
                         </div>
