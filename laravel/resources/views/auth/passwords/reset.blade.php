84679281c6c7f16a566e37a3b0c8f2272785a01a
Augusto <augustotemplar11@gmail.com>
2025-02-20 13:21:10 +0100

    update reset.blade.php

--- laravel/resources/views/auth/passwords/reset.blade.php
+++ laravel/resources/views/auth/passwords/reset.blade.php
@@ -2,33 +2,23 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-end">
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-header text-center">{{ __('Reset Password') }}</div>
-
-                <div class="card-body text-center">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="container" style="position: relative; z-index: 1; background-color: #6d6d6d; border-radius: 10px; padding: 20px;">
+                
+                <h1 class="display-4 text-center">Reset Password</h1>
+                <p class="lead text-center">Inserisci la nuova password</p>
+                
+                <div>
                     <form method="POST" action="{{ route('password.update') }}">
                         @csrf
-
+                        
                         <input type="hidden" name="token" value="{{ $token }}">
 
-                        <div class="row mb-3 justify-content-end">
-                            <div class="col-md-12">
-                                <input id="email" type="email" placeholder="Email Address" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3 justify-content-end">
-                            <div class="col-md-12">
-                                <input id="password" type="password" placeholder="Password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -36,16 +26,17 @@
                                 @enderror
                             </div>
                         </div>
-
-                        <div class="row mb-3 justify-content-end">
-                            <div class="col-md-12">
-                                <input id="password-confirm" type="password" placeholder="Confirm Password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                        
+                        <div class="row mb-3">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Conferma Password') }}</label>
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
-
-                        <div class="row mb-0 justify-content-end">
-                            <div class="col-md-12">
-                                <button type="submit" class="btn btn-primary">
+                        
+                        <div class="row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-custom-pri">
                                     {{ __('Reset Password') }}
                                 </button>
                             </div>
